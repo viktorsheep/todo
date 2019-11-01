dc9a6285b3852b0f3f53589e6d63f2568be663a7
Viktolo Romanof <malice.bloodymary@gmail.com>
2019-11-01 21:57:18 +0630

    home vue fix #2

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-        <h1 style="text-align: center;">Welcome to Viktodo. Simply login to check your todo list or register if you're new.</h1>
+        <h1 style="text-align: center; margin-top: 30px;">Welcome to Viktodo. Simply login to check your todo list or register if you're new.</h1>
     </div>
 </template>
 
