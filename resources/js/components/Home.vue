7784ba6ddcdfc76bf5f58f7cd09230a7562c394f
Viktolo Romanof <malice.bloodymary@gmail.com>
2019-11-01 21:53:32 +0630

    home page fix

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="container">
-        <h1>Hello from home page</h1>
-        <h2>Testing Please Work</h2>
+        <h1 style="text-align: center;">Welcome to Viktodo. Simply login to check your todo list or register if you're new.</h1>
     </div>
 </template>
 
